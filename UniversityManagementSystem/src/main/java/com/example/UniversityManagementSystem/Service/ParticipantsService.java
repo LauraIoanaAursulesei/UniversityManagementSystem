2f69a13da948126ba4eec6bf00ac948f69784e55
Laura <lauraioanaaursulesei@gmail.com>
2023-02-09 19:16:48 +0200

    Minor changes

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/Service/ParticipantsService.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/Service/ParticipantsService.java
@@ -1,6 +1,16 @@
 package com.example.UniversityManagementSystem.Service;
+import com.example.UniversityManagementSystem.Model.Participants;
+import com.example.UniversityManagementSystem.Repository.ParticipantsRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParticipantsService {
+
+    private ParticipantsRepository participantsRepository;
+
+    public ParticipantsService(ParticipantsRepository participantsRepository) {
+        this.participantsRepository = participantsRepository;
+    }
+
+    public Participants registerParticipant(Participants newParticipant)
 }
